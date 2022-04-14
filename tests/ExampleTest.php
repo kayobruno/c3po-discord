118a5b1707f6943f919089e2f35c0b72bdb8df31
Kayo Bruno <kayodw@gmail.com>
2022-04-13 23:33:26 -0300

    Add unit test configuration

---
+++ tests/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+}
