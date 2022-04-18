5f2c8d17dc3c51732a9012657edbc1c113cc3675
Kayo Bruno <kayodw@gmail.com>
2022-04-17 21:46:17 -0300

    Add In memory persistence

---
+++ src/Repositories/InMemoryReminderPersistence.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Contracts\Persistence;
+
+class InMemoryReminderPersistence implements Persistence
+{
+    private array $reminders = [];
+    private int $lastId = 0;
+
+    public function generateId(): int
+    {
+        $this->lastId++;
+        return $this->lastId;
+    }
+
+    public function all(): array
+    {
+        return $this->reminders;
+    }
+
+    public function find(int $id): array
+    {
+        return $this->reminders[$id];
+    }
+
+    public function update(int $id, array $params): void
+    {
+        $this->reminders[$id] = $params;
+    }
+
+    public function insert(array $params): void
+    {
+        $this->reminders[$this->generateId()] = $params;
+    }
+
+    public function delete(int $id): void
+    {
+        unset($this->reminders[$id]);
+    }
+}
